596af3e7a5498e079f85729d0e8b50bdea622213
Ciara Molloy <20089106@mail.wit.ie>
2022-11-02 17:35:00 +0000

    Fix backward/forward arrows.

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -5,35 +5,26 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
-import Avatar from "@mui/material/Avatar";
-import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useNavigate } from "react-router-dom";
 
 const MovieHeader = (props) => {
   const movie = props.movie;
-  const favouriteMovies = JSON.parse(localStorage.getItem("favourites"));
-  //TODO
+  const navigate = useNavigate();
+
   return (
-    <Paper
-      component="div"
-      sx={{
-        display: "flex",
-        justifyContent: "space-around",
-        flexWrap: "wrap",
-        padding: 1.5,
-        margin: 0,
-      }}
-    >
-      <IconButton aria-label="go back">
+    <Paper 
+        component="div" 
+        sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            flexWrap: "wrap",
+            padding: 1.5,
+            margin: 0,
+        }}
+      >
+      <IconButton aria-label="go back" onClick={() => navigate(-1)} >
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
-
-      {
-        favouriteMovies ? (
-          <Avatar sx={{ backgroundColor: 'red' }}>
-            <FavoriteIcon />
-          </Avatar>
-        ) : null
-      }
 
       <Typography variant="h4" component="h3">
         {movie.title}
@@ -43,7 +34,8 @@ const MovieHeader = (props) => {
         <br />
         <span sx={{ fontSize: "1.5rem" }}>{`   "${movie.tagline}"`} </span>
       </Typography>
-      <IconButton aria-label="go forward">
+
+      <IconButton aria-label="go forward" onClick={() => navigate(+1) } >
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
