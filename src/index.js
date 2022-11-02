ce6101e447a01530f66129e9d39ceeedcb19a4c8
Ciara Molloy <20089106@mail.wit.ie>
2022-11-02 20:20:27 +0000

    Add new review support.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import SiteHeader from './components/siteHeader';
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -29,6 +30,7 @@ const App = () => {
         <SiteHeader /> {/*New Header */}
         <MoviesContextProvider>
           <Routes>
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
             <Route path="/reviews/:id" element={<MovieReviewPage />} />
             <Route exact path="/movies/upcoming" element={<UpcomingMoviesPage />} />
             <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
